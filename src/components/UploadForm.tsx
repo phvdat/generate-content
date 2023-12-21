e7243866e416f37df494f8476025fb15b2496a76
phvdat <dat.pham@lfglobaltech.com>
2023-12-21 16:49:00 +0700

    update: setTimeout for api call

--- src/components/UploadForm.tsx
+++ src/components/UploadForm.tsx
@@ -3,6 +3,12 @@ import React, { ChangeEvent, useState } from 'react'
 import { sendMessage } from 'utils/sendMessage'
 import * as XLSX from 'xlsx'
 
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
 const UploadForm = () => {
   const [processedData, setProcessedData] = useState<any[]>([])
 
@@ -21,10 +27,10 @@ const UploadForm = () => {
         console.log(data)
         const processedData = await Promise.all(
           data.map(async (row: any) => {
+            await sleep(30000)
             const question: string = row['Prompt 1']
             const { data } = await sendMessage(question)
             console.log(data)
-
             const reply = data.message
             return { ...row, ChatGPTcontent: reply }
           })
